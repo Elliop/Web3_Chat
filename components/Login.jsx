a530f0bffb6fed731be6098858a4f7cbb5a19259
Elliop <abdelmouhayminegx@gmail.com>
2021-12-16 16:15:34 +0100

    Creating the Login Component

---
+++ components/Login.jsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <div className="bg-black relative text-white">
+      {/*  */}
+      Login screen
+    </div>
+  );
+};
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
+import Login from "../components/Login";
 
 export default function Home() {
+  const isAuthenticated = false;
+
+  if (!isAuthenticated) return <Login />;
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
