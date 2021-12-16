ac01f16b613a70af2392701916c018b86d97e61b
Elliop <abdelmouhayminegx@gmail.com>
2021-12-16 15:50:58 +0100

    Fisrt Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Metaverse</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome to Metaverse!</h1>
+    </div>
+  );
+}
