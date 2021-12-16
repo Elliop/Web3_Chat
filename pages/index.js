18ecb48dede0987469075902172e6275402a1797
Elliop <abdelmouhayminegx@gmail.com>
2021-12-16 18:03:54 +0100

    Building the Logout Functionality

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  const { isAuthenticated } = useMoralis();
+  const { isAuthenticated, logout } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
   return (
@@ -12,7 +12,15 @@ export default function Home() {
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to Metaverse!</h1>
+      <div className="flex flex-col justify-center items-center w-full h-screen space-y-4">
+        <h1>Welcome to Metaverse!</h1>
+        <button
+          className="py-2 px-6 font-bold rounded-lg text-white bg-cyan-700 animate-pulse"
+          onClick={logout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
