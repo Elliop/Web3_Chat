c06d11ef5c1c395d33324f460e9fad5e21632918
Elliop <abdelmouhayminegx@gmail.com>
2021-12-16 16:54:07 +0100

    Building the Login Component

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
 const Login = () => {
   return (
     <div className="bg-black relative text-white">
-      {/*  */}
-      Login screen
+      <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
+        <Image
+          className="object-cover rounded-full"
+          src="/Logo.jpg"
+          width={200}
+          height={200}
+        />
+        <button className="p-5 font-bold rounded-lg text-white bg-teal-600 animate-pulse">
+          Login to the Metaverse
+        </button>
+      </div>
+      <div className="w-full h-screen">
+        <Image src="/Bg.jpg" layout="fill" objectFit="cover" />
+      </div>
     </div>
   );
 };
