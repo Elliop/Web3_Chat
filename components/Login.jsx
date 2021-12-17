279d76ceab548d65164be89bd8616c874fc011eb
Elliop <abdelmouhayminegx@gmail.com>
2021-12-17 22:28:33 +0100

    Fix Animation Bug

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useMoralis } from "react-moralis";
+import { useState } from "react";
 
 const containerVariants = {
   hidden: {
@@ -16,8 +17,44 @@ const containerVariants = {
   },
 };
 
+const textVariants = {
+  hidden: {
+    opacity: 0,
+    y: 450,
+  },
+  visible: {
+    opacity: 1,
+    y: 10,
+    transition: { delay: 2, type: "spring", stiffness: 40 },
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: "easeInOut" },
+  },
+};
+
+const buttonVariants = {
+  hidden: {
+    opacity: 0,
+    y: 450,
+  },
+  visible: {
+    opacity: 1,
+    y: 10,
+    transition: { delay: 2.6, type: "spring", stiffness: 40 },
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: "easeInOut" },
+  },
+};
+
 const Login = () => {
   const { authenticate } = useMoralis();
+  const [animate, setAnimate] = useState(false);
+  setTimeout(() => {
+    setAnimate(true);
+  }, 5000);
   return (
     <div className="bg-black relative text-white">
       <div className="flex flex-col absolute z-50 h-4/5 w-full items-start justify-center space-y-4 space-x-8">
@@ -33,21 +70,22 @@ const Login = () => {
           Metalliop
         </motion.p>
         <motion.div
-          initial={{ y: 450 }}
-          animate={{ y: 10 }}
-          transition={{ delay: 2, type: "spring", stiffness: 40 }}
+          variants={textVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
         >
           <p className="font-bold text-xl text-slate-400">Private and Secure</p>
           <p className="font-bold text-xl text-slate-400">Own Your Data</p>
         </motion.div>
         <motion.button
-          variants={containerVariants}
-          initial={{ y: 450 }}
-          animate={{ y: 10 }}
-          transition={{ delay: 2.4, type: "spring", stiffness: 40 }}
+          variants={buttonVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
           onClick={authenticate}
-          className="py-4 font-bold rounded-lg text-white bg-gradient-to-r from-blue-400 to-pink-500 
-          animate-pulse hover:animate-none w-1/4"
+          className={`py-4 font-bold rounded-lg text-white bg-gradient-to-r from-blue-400 to-pink-500 
+          ${animate && "animate-pulse"} hover:animate-none w-1/4`}
         >
           Login
         </motion.button>
