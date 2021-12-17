405337d318c8e13168fa3267eb6d22c21f87a891
Elliop <abdelmouhayminegx@gmail.com>
2021-12-17 17:41:55 +0100

    Add Animations

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,17 +1,54 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { useMoralis } from "react-moralis";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 1, duration: 1.5 },
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: "easeInOut" },
+  },
+};
 
 const Login = () => {
   const { authenticate } = useMoralis();
   return (
     <div className="bg-black relative text-white">
-      <div className="flex flex-col absolute z-50 h-4/5 w-full items-start justify-center space-y-4">
-        <button
-          onClick={authenticate}
-          className="p-5 font-bold rounded-lg text-white bg-teal-600 animate-pulse hover:animate-none"
+      <div className="flex flex-col absolute z-50 h-4/5 w-full items-start justify-center space-y-4 space-x-8">
+        <div></div>
+        <motion.p
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          className="text-6xl font-black w-1/4 text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-pink-500"
         >
-          Login to the Metaverse
-        </button>
+          Welcome to Metalliop
+        </motion.p>
+        <motion.div
+          initial={{ y: 450 }}
+          animate={{ y: 10 }}
+          transition={{ delay: 2, type: "spring", stiffness: 40 }}
+        >
+          <p className="font-bold text-xl text-slate-400">Private and Secure</p>
+          <p className="font-bold text-xl text-slate-400">Own Your Data</p>
+        </motion.div>
+        <motion.button
+          initial={{ y: 450 }}
+          animate={{ y: 10 }}
+          transition={{ delay: 2.4, type: "spring", stiffness: 40 }}
+          onClick={authenticate}
+          className="py-4 font-bold rounded-lg text-white bg-gradient-to-r from-blue-400 to-pink-500 
+          animate-pulse hover:animate-none w-1/4"
+        >
+          Login
+        </motion.button>
       </div>
       <div className="w-full h-screen">
         <Image src="/moon-36.gif" layout="fill" objectFit="cover" />
